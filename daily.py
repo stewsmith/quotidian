c5dbf44e9513db835b6845f39e833645c97ed1a5
Stewart Smith <stewart@optimizely.com>
2014-12-06 13:46:11 -0500

    send simple email

--- daily.py
+++ daily.py
@@ -1,28 +1,51 @@
-import gmail
+import gmail, time, smtplib
 from secrets import gmail_username, gmail_password, gmail_email
 
-def get_senders(g):
+def get_users(g):
     emails = g.inbox().mail(prefetch=True)
 
-    senders = set()
+    users = set()
 
     for email in emails:
         email.fetch()
 
-        sender = email.fr
+        user = email.fr
 
         # remove noreply addresses
-        if 'noreply' in sender:
+        if 'noreply' in user:
             continue
 
-        senders.add(email.fr)
-    return senders
+        users.add(email.fr)
+    return users
 
-def get_mail_message(g):
+def get_mail_message(g, user):
+    msg = pick_old_message(g, user)
+    day = time.strftime("%A %b %d")
+    subj = 'It\'s ' + day + ' - How did your day go?'
+
+    message = 'Subject: %s\n\n%s' % (subj, msg)
+    return message
+
+def pick_old_message(g, user):
     pass
+
+
+def send_message(user, message):
+    from_address = gmail_email
+    to_address = user
+
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(gmail_username, gmail_password)
+    server.sendmail(from_address, to_address, message)
+    server.quit()
 
 if __name__ == '__main__':
     g = gmail.login(gmail_username, gmail_password)
-    senders = get_senders(g)
+    users = get_users(g)
 
-    print senders
+    for user in users:
+        message = get_mail_message(g, user)
+        send_message(user, message)
+
+    print users
